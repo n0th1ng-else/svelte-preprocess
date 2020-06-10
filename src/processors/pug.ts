366b420ba2d50d07d177b8588088337a7d7ca0e2
Christian Kaisermann <christian@kaisermann.me>
2020-06-10 20:15:27 -0300

    Remove type imports

--- src/processors/pug.ts
+++ src/processors/pug.ts
@@ -1,4 +1,4 @@
-import type { Options, PreprocessorGroup } from '../types';
+import { Options, PreprocessorGroup } from '../types';
 
 export default (options?: Options.Pug): PreprocessorGroup => ({
   async markup({ content, filename }) {
